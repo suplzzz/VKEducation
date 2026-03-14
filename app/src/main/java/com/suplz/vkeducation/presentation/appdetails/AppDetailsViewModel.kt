b4ba76555863c1406743981105e1a9010b5f31b8
suplzzz <dualspeed34@gmail.com>
2026-03-14 21:21:23 +0300

    fix AppDetailsViewModel.kt

--- app/src/main/java/com/suplz/vkeducation/presentation/appdetails/AppDetailsViewModel.kt
+++ app/src/main/java/com/suplz/vkeducation/presentation/appdetails/AppDetailsViewModel.kt
@@ -2,19 +2,23 @@ package com.suplz.vkeducation.presentation.appdetails
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.suplz.vkeducation.domain.appdetails.AppDetails
-import com.suplz.vkeducation.domain.Category
+import com.suplz.vkeducation.data.appdetails.AppDetailsRepositorImpl
+import com.suplz.vkeducation.domain.appdetails.GetAppDetailsUseCase
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.BUFFERED
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlin.time.Duration.Companion.seconds
 
 class AppDetailsViewModel : ViewModel() {
+
+    private val getAppDetailsUseCase = GetAppDetailsUseCase(
+        // Подстановку реализации должен делать DI.
+        // Будет доработано в следующих лекциях.
+        appDetailsRepository = AppDetailsRepositorImpl(),
+    )
 
     private val _state = MutableStateFlow<AppDetailsState>(AppDetailsState.Loading)
     val state = _state.asStateFlow()
@@ -47,26 +51,7 @@ class AppDetailsViewModel : ViewModel() {
             _state.value = AppDetailsState.Loading
 
             runCatching {
-                // Эмулируем загрузку с бэкенда
-                delay(2.seconds)
-
-                // В будущем заменим этот метод на вызов API.
-                val appDetails = AppDetails(
-                    name = "Гильдия Героев: Экшен ММО РПГ",
-                    developer = "VK Play",
-                    category = Category.GAME,
-                    ageRating = 12,
-                    size = 223.7f,
-                    screenshotUrlList = listOf(
-                        "https://static.rustore.ru/imgproxy/-y8kd-4B6MQ-1OKbAbnoAIMZAzvoMMG9dSiHMpFaTBc/preset:web_scr_lnd_335/plain/https://static.rustore.ru/apk/393868735/content/SCREENSHOT/dfd33017-e90d-4990-aa8c-6f159d546788.jpg@webp",
-                        "https://static.rustore.ru/imgproxy/dZCvNtRKKFpzOmGlTxLszUPmwi661IhXynYZGsJQvLw/preset:web_scr_lnd_335/plain/https://static.rustore.ru/apk/393868735/content/SCREENSHOT/60ec4cbc-dcf6-4e69-aa6f-cc2da7de1af6.jpg@webp",
-                        "https://static.rustore.ru/imgproxy/g5whSI1uNqaL2TUO7TFfM8M63vXpWXNCm2vlX4Ahvc4/preset:web_scr_lnd_335/plain/https://static.rustore.ru/apk/393868735/content/SCREENSHOT/c2dde8bc-c4ab-482a-80a5-2789149f598d.jpg@webp",
-                        "https://static.rustore.ru/imgproxy/TjeurtC7BczOVJt74XhjGYuQnG1l4rx6zpDqyMb00GY/preset:web_scr_lnd_335/plain/https://static.rustore.ru/apk/393868735/content/SCREENSHOT/08318f76-7a9c-43aa-b4a7-1aa878d00861.jpg@webp",
-                    ),
-                    iconUrl = "https://static.rustore.ru/imgproxy/APsbtHxkVa4MZ0DXjnIkSwFQ_KVIcqHK9o3gHY6pvOQ/preset:web_app_icon_62/plain/https://static.rustore.ru/apk/393868735/content/ICON/3f605e3e-f5b3-434c-af4d-77bc5f38820e.png@webp",
-                    description = "Легендарный рейд героев в Фэнтези РПГ. Станьте героем гильдии и зразите мастера подземелья!"
-
-                )
+                val appDetails = getAppDetailsUseCase()
 
                 _state.value = AppDetailsState.Content(
                     appDetails = appDetails,
