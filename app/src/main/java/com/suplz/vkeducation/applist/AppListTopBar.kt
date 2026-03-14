d4a052132e8f762d1389d38128903c43fb56d17b
suplzzz <dualspeed34@gmail.com>
2026-03-14 20:01:43 +0300

    add preview for AppListTopBar.kt

--- app/src/main/java/com/suplz/vkeducation/applist/AppListTopBar.kt
+++ app/src/main/java/com/suplz/vkeducation/applist/AppListTopBar.kt
@@ -16,9 +16,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.suplz.vkeducation.R
 import com.suplz.vkeducation.ui.theme.RuStoreWhite
+import com.suplz.vkeducation.ui.theme.VKEducationTheme
 
 @Composable
 fun AppListTopBar(
@@ -59,5 +61,12 @@ fun AppListTopBar(
             }
 
         }
+    }
+}
+@Preview
+@Composable
+private fun Preview() {
+    VKEducationTheme {
+        AppListTopBar()
     }
 }
